21f514dd61962735f2d624705de52eb04da046e9
Ryan Roman <rtroman14@gmail.com>
2020-08-23 20:06:30 -0600

    Updates

---
+++ precache-manifest.b9fafe46cb44d5f2819773af8ec7d7d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0decefb96e8918b51fc1eb3604e70f41",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a2ad373c76621db93232",
+    "url": "/monsters-rolodex/static/css/main.4692bd7b.chunk.css"
+  },
+  {
+    "revision": "ddfc98fd1fb7cc1baeab",
+    "url": "/monsters-rolodex/static/js/2.fcb60162.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.fcb60162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ad373c76621db93232",
+    "url": "/monsters-rolodex/static/js/main.4f749d6a.chunk.js"
+  },
+  {
+    "revision": "4b766f3713c9b2c594ff",
+    "url": "/monsters-rolodex/static/js/runtime-main.6b478adf.js"
+  }
+]);
